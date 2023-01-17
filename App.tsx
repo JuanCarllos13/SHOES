e2ea98d49e65dee0f3b9cf14c354fa24de9d2cf7
Juan Carlos <juan2017amaral@gmail.com>
2023-01-17 17:06:56 -0400

    onesignal

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
+
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,6 +10,9 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+
+OneSignal.setAppId('a46a5685-a797-4f4b-b2dc-ab9510cc2ad1');
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
